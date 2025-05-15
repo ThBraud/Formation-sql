f56f953f71782592e03c81ca427233fee8ff0f4d
ThBraud <thomas.braud04@gmail.com>
2025-05-15 13:39:44 +0200

    Add files via upload

---
+++ Travaux Pratiques/Second Travaux Pratiques/TP 13-05-25 Requetes ,Bonus et Analyse.sql
@@ -0,0 +1,24 @@
+EXPLAIN SELECT * FROM Clients;
+EXPLAIN SELECT * FROM produits;
+EXPLAIN SELECT * FROM ligne_commandes;
+EXPLAIN SELECT * FROM historique_commandes;
+EXPLAIN SELECT * FROM Commandes;
+
+
+#SELECT * FROM Clients ORDER BY nom;
+
+#SELECT * FROM Produits WHERE stock > 50;
+
+#SELECT * FROM historique_commandes ORDER BY montant_total DESC;
+
+#SELECT Clients.nom, Commandes.date_commande, SUM(Lignes_Commande.total) AS Total_par_Clients FROM Clients 
+#JOIN Commandes ON Clients.client_id = Commandes.client_id 
+#JOIN Lignes_Commande ON Commandes.commande_id = Lignes_Commande.commande_id GROUP BY nom
+
+USE stock;
+#SELECT COUNT(*) AS nombre_lignes FROM Clients WHERE client_id IS NOT NULL
+#SELECT COUNT(*) AS nombre_lignes FROM Commandes WHERE commande_id IS NOT NULL
+#SELECT COUNT(*) AS nombre_lignes FROM historique_commandes WHERE historique_id IS NOT NULL
+
+#SELECT COUNT(*) AS nombre_lignes FROM lignes_commande WHERE ligne_id IS NOT NULL
+#SELECT COUNT(*) AS nombre_lignes FROM produits WHERE produit_id IS NOT NULL
